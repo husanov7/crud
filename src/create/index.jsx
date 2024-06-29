50b563502e37266aa285f945ded214d995020a19
mohira-s <mohira-s@gmail.com>
2024-06-29 11:32:38 +0100

    yjjyjyy

--- src/create/index.jsx
+++ src/create/index.jsx
@@ -13,11 +13,11 @@ export default function Create() {
     <div>
       <h1>create new tavar</h1>
       <form onSubmit={handleSubmit(newTavar)}>
-        <input type="text" placeholder="ism" {...register("example")} />
-        <input type="text" placeholder="familya" {...register("example")} />
-        <input type="text" placeholder="age" {...register("example")} />
-        <input type="text" placeholder="language" {...register("example")} />
-        <input type="text" placeholder="location" {...register("example")} />
+        <input type="text" placeholder="ism" {...register("ism")} />
+        <input type="text" placeholder="familya" {...register("familya")} />
+        <input type="text" placeholder="age" {...register("age")} />
+        <input type="text" placeholder="language" {...register("language")} />
+        <input type="text" placeholder="location" {...register("location")} />
         <button>add</button>
       </form>
     </div>
