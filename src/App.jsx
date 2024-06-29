31a4f41efd49237f7af38192ef2b963c4d2b62b2
mohira-s <mohira-s@gmail.com>
2024-06-29 11:17:36 +0100

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { Typography, Card } from "@material-tailwind/react";
+import { Link, Route, Router, Routes } from "react-router-dom";
+import Create from "./create";
 
 export default function App() {
   return (
    <>
+     {/* <Link to={"/create"} >create</Link> */}
+  <Link to={"/create"} >create</Link>
+
+  <Routes>
+    {/* <Route path="/create" element={<Create/>} /> */}
+    <Route path="/create" element={<Create/>} />
+
+  </Routes>
+
    </>
   );
 }
